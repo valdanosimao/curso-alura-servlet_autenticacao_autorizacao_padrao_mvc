8bafaed3f8c2d6467a32d59e0e5ecb50e0df119e
Valdano Simão <valdanosimao@gmail.com>
2022-05-04 17:28:53 -0300

    implementando o primeiro filtro

--- src/main/java/br/com/alura/gerenciador/acao/ListaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresa.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -16,12 +15,7 @@ public class ListaEmpresa implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}		
-		
+				
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
